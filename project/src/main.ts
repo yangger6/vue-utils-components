cabcdfd32a4415f1794a8d793af4c734958bfb14
chenyang <chenyang@shanhs.com>
2019-04-02 17:58:34 +0800

    fix bug

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,10 +1,10 @@
 import Vue, * as vue from 'vue'
-// import VueUtilsComponents from '../../src'
-import {VOssImg} from '../../src'
+import VueUtilsComponents from '../../src'
+// import {VOssImg} from '../../src'
 import App from './App.vue'
 
-// Vue.use(VueUtilsComponents)
-Vue.use(VOssImg)
+Vue.use(VueUtilsComponents)
+// Vue.use(VOssImg)
 const Sample: vue.Component = App
 
 window.addEventListener('load', () => {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import VOssImg from './components/v-oss-img'
 import Vue, {PluginObject} from 'vue'
-const version = '0.0.1';
+const version = '0.0.5'
+
+interface ICustomWindow {
+    Vue: typeof Vue
+}
+declare let window: ICustomWindow
 
 const components: {
     [key: string]: PluginObject<{
@@ -12,10 +17,15 @@ const components: {
 }
 
 function install(vue: typeof Vue, options = {}) {
-    Object.keys(components).forEach(key => {
+    Object.keys(components).forEach((key: string) => {
         Vue.use(components[key])
     })
 }
+
+if (typeof window !== 'undefined' && window.Vue) {
+    install(window.Vue)
+}
+
 export {
     VOssImg
 }
