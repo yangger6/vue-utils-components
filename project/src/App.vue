1e609db76760f541bd265b4ceb1e4988964b8a54
chenyang <chenyang@shanhs.com>
2019-04-23 19:11:51 +0800

    fix oss option bug

--- project/src/App.vue
+++ project/src/App.vue
@@ -2,7 +2,7 @@
     <div>
         <v-oss-img src="https://shanhs.oss-cn-shenzhen.aliyuncs.com/newboss/2019-03-28/ae083297-c424-4b3b-b966-b02d341f3e97.webp"></v-oss-img>
         <v-oss-img src="https://shanhs.oss-cn-shenzhen.aliyuncs.com/newboss/2019-03-28/00c69974-ecb2-400e-806b-4d177c844b24.jpg"></v-oss-img>
-        <v-oss-img src="https://shanhs.oss-cn-shenzhen.aliyuncs.com/test/offline/assets/image/2019/4/23/1556016925302_207174064.png?x-oss-process=image/resize,l_690" alt=""></v-oss-img>
+        <v-oss-img src="https://shanhs.oss-cn-shenzhen.aliyuncs.com/newboss/2019-03-28/00c69974-ecb2-400e-806b-4d177c844b24.jpg?x-oss-process=image/resize,l_690" alt=""></v-oss-img>
     </div>
 </template>
 <script lang="ts">
