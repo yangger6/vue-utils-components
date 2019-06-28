c2287eccddae6fbba1528ef047bd6f1ee1f290a6
chenyang <chenyang@shanhs.com>
2019-06-28 11:34:08 +0800

    fix bug

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <v-oss-img src="https://shanhs.oss-cn-shenzhen.aliyuncs.com/newboss/2019-03-28/ae083297-c424-4b3b-b966-b02d341f3e97.webp"></v-oss-img>
+        <v-oss-img :width="300" height="300px" src="https://shanhs.oss-cn-shenzhen.aliyuncs.com/newboss/2019-03-28/ae083297-c424-4b3b-b966-b02d341f3e97.webp"></v-oss-img>
         <v-oss-img src="https://shanhs.oss-cn-shenzhen.aliyuncs.com/newboss/2019-03-28/00c69974-ecb2-400e-806b-4d177c844b24.jpg"></v-oss-img>
-        <v-oss-img src="https://shanhs.oss-cn-shenzhen.aliyuncs.com/newboss/2019-03-28/00c69974-ecb2-400e-806b-4d177c844b24.jpg?x-oss-process=image/resize,l_690" alt=""></v-oss-img>
+        <v-oss-img width="300" src="https://shanhs.oss-cn-shenzhen.aliyuncs.com/newboss/2019-03-28/00c69974-ecb2-400e-806b-4d177c844b24.jpg?x-oss-process=image/resize,l_690" alt=""></v-oss-img>
     </div>
 </template>
 <script lang="ts">
--- src/components/v-oss-img/index.vue
+++ src/components/v-oss-img/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <img ref="img" :src="currentSrc" @error="loadFallback" :alt="alt" @click="$emit('click')">
+    <img ref="img" :src="currentSrc" @error="loadFallback" :alt="errorText || alt" @click="$emit('click')" :width="currentWidth" :height="currentHeight">
 </template>
 <script lang="ts">
     import {Vue, Prop, Component} from 'vue-property-decorator'
@@ -10,8 +10,12 @@
             default: ''
         }) src: string
         @Prop(String) alt!: string
+        @Prop(String | Number) width!: string | number
+        @Prop(String | Number) height!: string | number
         isOss: boolean = false
         isWebp: boolean = false
+        errorText: string = ''
+        errorTimes: number = 0
         $supportWebp: boolean = Vue.prototype.$supportWebp
         created() {
             if (this.src) {
@@ -21,6 +25,11 @@
         }
         loadFallback(e: EventTarget) {
             const img: HTMLImageElement = (this.$refs as any).img
+            if (this.errorTimes >= 5) { // 5 times errorFallback
+                const isChinese = new Date().toLocaleString().match(/\p{Unified_Ideograph}/u)
+                this.errorText = isChinese ? '图片加载失败' : 'image loaded error, please check image url'
+                return
+            }
             if (this.$supportWebp && !this.isWebp) { // fallback image
                 img.src = this.src
             } else { // image loaded error
@@ -28,22 +37,51 @@
                     img.src = 'error.png'
                 }
             }
+            this.errorTimes ++
         }
         get currentSrc () {
+            let resizeText = `${this.currentWidth ? `,w_${this.currentWidth}` : ''}${this.currentHeight ? `,h_${this.currentHeight}` : ''}`
+            http://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg?x-oss-process=image/resize,l_100
             if (this.isOss) {
                 if (this.$supportWebp) {
                     if (this.isWebp) {
-                        return this.src
+                        return `${this.src}?x-oss-process=image/resize${resizeText}`
                     }
                     if (/x-oss-process/.exec(this.src)) { // oss已经使用了参数的情况 追加选项即可
-                        return this.src + ',image/format,webp'
+                        return this.src + ',image/format,webp' + resizeText
                     }
-                    return this.src + '?x-oss-process=image/format,webp'
+                    return this.src + '?x-oss-process=image/format,webp,resize' + resizeText
                 } else {
-                    return this.src + '?x-oss-process=image/interlace,1/format,jpg'
+                    return this.src + '?x-oss-process=image/interlace,1/format,jpg,resize' + resizeText
                 }
             } else {
                 return this.src
+            }
+        }
+        get currentWidth (): string {
+            if (this.width) {
+                if (typeof this.width === 'string') {
+                    return this.width.match(/\d*/) ? this.width.match(/\d*/)[0] : ''
+                } else if (typeof this.width === 'number') {
+                    return String(this.width)
+                } else {
+                    return ''
+                }
+            } else {
+                return ''
+            }
+        }
+        get currentHeight (): string  {
+            if (this.height) {
+                if (typeof this.height === 'string') {
+                    return this.height.match(/\d*/) ? this.height.match(/\d*/)[0] : ''
+                } else if (typeof this.height === 'number') {
+                    return String(this.height)
+                } else {
+                    return ''
+                }
+            } else {
+                return ''
             }
         }
     }
